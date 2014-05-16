5cc9ae8fdd4af4101e9abb2ef70b3acbfa5337a6
Dominik Richter <dominik.richter@gmail.com>
2014-05-16 13:56:03 +0200

    improvement: make merging deep-merge on maps
    also extract the code into its own module.
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- lib/puppet/parser/functions/merge_hardening.rb
+++ lib/puppet/parser/functions/merge_hardening.rb
@@ -2,38 +2,46 @@ Puppet::Parser::Functions::newfunction(:merge_hardening, :type => :rvalue) do |a
   user_params = args[0]
   hardening_params = args[1]
 
-  # merge hashes
-  if user_params.instance_of? Hash and
-     hardening_params.instance_of? Hash
-
-     # do a manual merge since we need to see what is set
-     # or gets overwritten
-     hardening_params.each do |k,v|
-       # only set the hardening parameter, if the user
-       # didn't define it himself
-       if not user_params.key? k
-         user_params[k] = v
-       end
-
-       if user_params[k] != hardening_params[k]
-         Puppet.warning "Hardening parameters: you already defined '#{k}' as '#{user_params[k]}' instead of '#{hardening_params[k]}'."
-       end
-     end
-
-
-  else
-
-    # merge simple values
-    if user_params.nil?
-      user_params = hardening_params
-    end
-
-    if user_params != hardening_params
-      Puppet.warning "Hardening parameters: you already defined '#{user_params}' instead of '#{hardening_params}'."
-    end
-
-  end
-  
   # return the merged value
-  user_params
+  HardeningStdlib.deep_merge_hardening user_params, hardening_params
+end
+
+module HardeningStdlib
+  extend self
+
+  def deep_merge_hardening user_param, hardening_param
+    # merge hashes
+    if user_param.instance_of? Hash and
+      hardening_param.instance_of? Hash
+
+      # do a manual merge since we need to see what is set
+      # or gets overwritten
+      hardening_param.each do |k,v|
+        # only set the hardening parameter, if the user
+        # didn't define it himself
+        if not user_param.key? k
+          user_param[k] = v
+          next
+        end
+
+        user_param[k] = deep_merge_hardening user_param[k], hardening_param[k]
+      end
+
+    else
+
+      # merge simple values
+      if user_param.nil?
+        user_param = hardening_param
+      end
+
+      if user_param != hardening_param
+        Puppet.warning "Hardening parameters: you already defined '#{user_param}' instead of '#{hardening_param}'."
+      end
+
+    end
+
+    # finally return the merged result
+    user_param
+  end
+
 end
